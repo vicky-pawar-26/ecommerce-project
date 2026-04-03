261a4614a51a14df18dc1893cfb6d4c6ea0853f2
vicky <pkpawar26@gmail.com>
2026-04-04 01:31:28 +0530

    Move orders and tracking into the react

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router';
 import './App.css'
 import { HomePage } from './Pages/HomePage';
 import { CheckOutPage } from './Pages/CheckOutPage';
+import { Orders } from './Pages/Orders';
+import { Tracking } from './Pages/Tracking';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/tracking" element={<Tracking />} />
     </Routes>
   );
 }
