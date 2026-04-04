752b8546fe7f2195cc3f448403ad0b3676bd82fb
vicky <pkpawar26@gmail.com>
2026-04-04 11:59:31 +0530

    change vite config

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -10,11 +10,11 @@ export function HomePage() {
 
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products').then((response) => {
+        axios.get('/api/products').then((response) => {
             setProducts(response.data);
         });
 
-        axios.get('http://localhost:3000/api/cart-items').then((response) => {
+        axios.get('/api/cart-items').then((response) => {
             setCart(response.data);
         });
     }, [])
